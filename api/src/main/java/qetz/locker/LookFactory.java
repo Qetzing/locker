f0f2bb6431b1755aedef39eb79fe2a03bc376698
qetzing <general@qetz.de>
2023-06-18 10:52:00 +0200

    Add documentation to LookFactory

--- api/src/main/java/qetz/locker/LookFactory.java
+++ api/src/main/java/qetz/locker/LookFactory.java
@@ -1,5 +1,11 @@
 package qetz.locker;
 
 public interface LookFactory {
+  /**
+   * Creates a Look for a player by the given {@link Outfit}.
+   *
+   * @param original The original {@link Outfit} of a player
+   * @return The {@link Look} for the player given by the original outfit
+   */
   Look create(Outfit original);
 }
